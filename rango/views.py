4cd15103d1abb6fadc868a4016cffa7f4c394c28
Sam Thompson <2759520t@student.gla.ac.uk>
2024-02-02 16:43:10 +0000

    added cat

--- rango/views.py
+++ rango/views.py
@@ -11,4 +11,10 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. \n <a href='/rango/'>Index</a>")
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage matches to {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': 'This tutorial has been put together by Sam Thompson'}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/about.html', context=context_dict)
